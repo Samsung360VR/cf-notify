5dd778a12baa7f34304048bc35c2a697445e2970
Dmitry Strygin <strygin@gmail.com>
2017-06-28 09:54:32 -0500

    Patched for SVR

--- lambda_notify.py
+++ lambda_notify.py
@@ -10,28 +10,38 @@ from itertools import groupby
 # Mapping CloudFormation status codes to colors for Slack message attachments
 # Status codes from http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-describing-stacks.html
 STATUS_COLORS = {
-    'CREATE_COMPLETE': 'good',
-    'CREATE_IN_PROGRESS': 'good',
+    'CREATE_COMPLETE': '#008000',
+    'CREATE_IN_PROGRESS': '#ccffcc',
     'CREATE_FAILED': 'danger',
-    'DELETE_COMPLETE': 'good',
+    'DELETE_COMPLETE': '#008000',
     'DELETE_FAILED': 'danger',
-    'DELETE_IN_PROGRESS': 'good',
+    'DELETE_IN_PROGRESS': '#00ff80',
     'ROLLBACK_COMPLETE': 'warning',
     'ROLLBACK_FAILED': 'danger',
     'ROLLBACK_IN_PROGRESS': 'warning',
-    'UPDATE_COMPLETE': 'good',
-    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS': 'good',
-    'UPDATE_IN_PROGRESS': 'good',
+    'UPDATE_COMPLETE': '#008000',
+    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS': '#33ff33',
+    'UPDATE_IN_PROGRESS': '#ccffcc',
     'UPDATE_ROLLBACK_COMPLETE': 'warning',
     'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS': 'warning',
     'UPDATE_ROLLBACK_FAILED': 'danger',
     'UPDATE_ROLLBACK_IN_PROGRESS': 'warning'
 }
 
-# List of CloudFormation status that will trigger a call to `get_stack_summary_attachment`
+# List of CloudFormation statuses that will trigger a call to `get_stack_summary_attachment`
 DESCRIBE_STACK_STATUS = [
     'CREATE_COMPLETE',
+    'DELETE_IN_PROGRESS',
+]
+
+# List of CloudFormation statuses that will trigger a call to `get_stack_params_attachment`
+DESCRIBE_STACK_PARAMS = [
+    'UPDATE_IN_PROGRESS'
+    'UPDATE_COMPLETE',
+    'CREATE_IN_PROGRESS',
+    'CREATE_COMPLETE',
     'DELETE_IN_PROGRESS'
+    'DELETE_COMPLETE',
 ]
 
 # List of properties from ths SNS message that will be included in a Slack message
@@ -40,8 +50,25 @@ SNS_PROPERTIES_FOR_SLACK = [
     'StackName',
 ]
 
+# List of parameters that will be included in a Slack message
+STACK_PARAMETERS_FOR_SLACK = [
+    'SubdomainName',
+    'aBlueOrGreen',
+    'environment',
+    'SubdomainName',
+    'version',
+    'configAppDomain',
+    'configAwsBucketName',
+    'configRTMPIngestAddress',
+]
+
+client = None
+
 
 def lambda_handler(event, context):
+    global client
+    client = boto3.client('cloudformation')
+
     message = event['Records'][0]['Sns']
     sns_message = message['Message']
     cf_message = dict(token.split('=', 1) for token in shlex.split(sns_message))
@@ -60,15 +87,16 @@ def get_stack_update_message(cf_message):
     attachments = [
         get_stack_update_attachment(cf_message)
     ]
-
-    if cf_message['ResourceStatus'] in DESCRIBE_STACK_STATUS:
+    resourse_status = cf_message['ResourceStatus']
+    #if resourse_status in DESCRIBE_STACK_PARAMS:
+    if not resourse_status.upper().endswith('_IN_PROGRESS'):
+        attachments.append(get_stack_params_attachment(cf_message['StackName']))
+    if resourse_status in DESCRIBE_STACK_STATUS:
         attachments.append(get_stack_summary_attachment(cf_message['StackName']))
 
     stack_url = get_stack_url(cf_message['StackId'])
 
     message = {
-        'icon_emoji': ':cloud:',
-        'username': 'cf-bot',
         'text': 'Stack: {stack} has entered status: {status} <{link}|(view in web console)>'.format(
                 stack=cf_message['StackName'], status=cf_message['ResourceStatus'], link=stack_url),
         'attachments': attachments
@@ -97,16 +125,14 @@ def get_stack_update_attachment(cf_message):
         status=cf_message['ResourceStatus'])
 
     return {
-        'fallback': title,
         'title': title,
         'fields': [{'title': k, 'value': v, 'short': True}
-                   for k, v in cf_message.iteritems() if k in SNS_PROPERTIES_FOR_SLACK],
+                   for k, v in cf_message.iteritems() if k.lower() in map(str.lower, SNS_PROPERTIES_FOR_SLACK)],
         'color': STATUS_COLORS.get(cf_message['ResourceStatus'], '#000000'),
     }
 
 
 def get_stack_summary_attachment(stack_name):
-    client = boto3.client('cloudformation')
     resources = client.describe_stack_resources(StackName=stack_name)
     sorted_resources = sorted(resources['StackResources'], key=lambda res: res['ResourceType'])
     grouped_resources = groupby(sorted_resources, lambda res: res['ResourceType'])
@@ -115,10 +141,41 @@ def get_stack_summary_attachment(stack_name):
     title = 'Breakdown of all {} resources'.format(len(resources['StackResources']))
 
     return {
-        'fallback': title,
         'title': title,
-        'fields': [{'title': 'Type {}'.format(k), 'value': 'Total {}'.format(v), 'short': True}
+        'fields': [{'title': 'Type {}'.format(k), 'value': 'Total {}'.format(v), 'short': False}
                    for k, v in resource_count.iteritems()]
+    }
+
+
+def get_stack_params_attachment(stack_name):
+    params = client.describe_stacks(StackName=stack_name).get('Stacks', {})[0].get('Parameters', [])
+    if not params:
+        return None
+
+    params = {e['ParameterKey']: e['ParameterValue'] for e in params}
+
+    if params.get('environment'):
+        if params['environment'].lower() == 'stage':
+            params['environment'] += ' :stage:'
+        elif params['environment'].lower() == 'production':
+            params['environment'] += ' :production:'
+        else:
+            params['environment'] += ' :construction:'
+
+    if params.get('aBlueOrGreen'):
+        if params['aBlueOrGreen'].lower() == 'blue':
+            params['aBlueOrGreen'] += ' :large_blue_circle:'
+        elif params['aBlueOrGreen'].lower() == 'green':
+            params['aBlueOrGreen'] += ' :green_apple:'
+        else:
+            params['aBlueOrGreen'] += ' :alien:'
+
+    title = 'Some parameters:'
+
+    return {
+        'title': title,
+        'fields': [{'title': k, 'value': v, 'short': False}
+                   for k, v in params.iteritems() if k.lower() in map(str.lower, STACK_PARAMETERS_FOR_SLACK)]
     }
 
 
